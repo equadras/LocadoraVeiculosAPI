3a80072729f6a95137c19b7b3249da77b16531d0
leonardonm15 <leonardonm15@gmail.com>
2024-04-07 20:28:48 -0300

    going

--- app.py
+++ app.py
@@ -1,19 +1,25 @@
-impot psycopg2
 import sys
-import ppri
 from flask import Flask
+from psutil import process_iter
+from signal import SIGTERM
+from sqlalchemy import create_engine
 
-app = Flask(_name_)
-conn_string = "postgresql://trab_banco_owner:5kYVI6gRfHlK@ep-nameless-tooth-a5fq2x2q.us-east-2.aws.neon.tech/trab_banco?sslmode=require"
-conn = psycopg2.connect(conn_string)
-cursor = conn.cursor()
+app = Flask(__name__)
 
-@app.route("/get_funcionario/<int: id_func>", methods["GET"])
+@app.route("/get_funcionario/<int:id_func>", methods=["GET"])
 def get_func(id_func):
-    return "<p>Hello, World!</p>"
+    with engine.connect() as connection:
+        result = connection.execute(f"SELECT * FROM funcionario WHERE id = {id_func}")
+        funcionario = result.fetchone()
+    if funcionario:
+        return f"Funcionário ID: {funcionario[0]}, Nome: {funcionario[1]}, Sobrenome: {funcionario[2]}"
+    else:
+        return "Funcionário não encontrado"
 
 @app.route("/")
 def hello_world():
+    return "Hello, World!"
 
-def main():
-
+if __name__ == "__main__":
+    engine = create_engine("postgresql://trab_banco_owner:5kYVI6gRfHlK@ep-nameless-tooth-a5fq2x2q-pooler.us-east-2.aws.neon.tech/trab_banco?sslmode=require")
+    app.run(debug=True, port=8080)
